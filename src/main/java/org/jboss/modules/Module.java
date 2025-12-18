2f4af5d05e193e321dfc71dbfc9218e232e8b728
Jean-Francois Denise <jfdenise@gmail.com>
2025-12-18 16:15:08 +0100

    Introduce a Cache class

--- src/main/java/org/jboss/modules/Module.java
+++ src/main/java/org/jboss/modules/Module.java
@@ -22,13 +22,11 @@ import static java.security.AccessController.doPrivileged;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.annotation.Annotation;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.AccessController;
@@ -50,6 +48,7 @@ import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Predicate;
+import org.jboss.modules.ClassCache.DefaultClassCache;
 
 import org.jboss.modules._private.ModulesPrivateAccess;
 import org.jboss.modules.filter.ClassFilter;
@@ -80,7 +79,6 @@ public final class Module {
     private static final MethodType MAIN_METHOD_TYPE = MethodType.methodType(void.class, String[].class);
     private static final MethodType POST_MAIN_METHOD_TYPE = MethodType.methodType(void.class);
 
-    private final static Set<String> RECORDED_MODULES = new HashSet<>();
 
     static {
         log = NoopModuleLogger.getInstance();
@@ -131,16 +129,6 @@ public final class Module {
                 return null;
             }
         });
-        String mods = System.getProperty("org.jboss.modules.record.classes.of");
-        if (mods != null) {
-            String[] split = mods.split(",");
-            for (String s : split) {
-                s = s.trim();
-                if (!s.isEmpty()) {
-                    RECORDED_MODULES.add(s);
-                }
-            }
-        }
     }
 
     // static properties
@@ -239,6 +227,7 @@ public final class Module {
     private MethodHandle mainMethod;
     private Set<String> recordedServices = new HashSet<>();
     private List<PermissionData> permissions = new ArrayList<>();
+    private ClassCache classCache;
     /**
      * Construct a new instance from a module specification.
      *
@@ -263,8 +252,17 @@ public final class Module {
         ModuleClassLoader moduleClassLoader = null;
         if (factory != null) moduleClassLoader = factory.create(configuration);
         if (moduleClassLoader == null) moduleClassLoader = new ModuleClassLoader(configuration);
-        //System.out.println("MODULE CLASSLOADER " + moduleClassLoader);
         this.moduleClassLoader = moduleClassLoader;
+        classCache = ClassCache.DEFAULT;
+    }
+
+    public ClassCache getCache() {
+        return classCache;
+    }
+
+    public void setClassCache(ClassCache classCache) {
+        this.classCache = classCache;
+        this.classCache.setModule(this);
     }
 
     public void cleanupPermissions() throws Exception {
@@ -1247,149 +1245,6 @@ public final class Module {
      */
     public PermissionCollection getPermissionCollection() {
         return permissionCollection;
-    }
-    
-    private Map<String, Class<?>> CACHE = new HashMap<>();
-    private Map<Class<?>, List<Object>> SERVICES = new HashMap<>();
-    private Map<String, Constructor> CONSTRUCTORS = new HashMap<>();
-    private Map<Class<?>, Map<Class<?>, Annotation>> ANNOTATIONS = new HashMap<>();
-    private Map<Class<?>, Map<Method, Map<Class<?>, Annotation>>> METHOD_ANNOTATIONS = new HashMap<>();
-    private Map<Class<?>, Map<Method, Annotation[][]>> PARAMETERS_ANNOTATIONS = new HashMap<>();
-    private Map<Class<?>, Method[]> METHODS = new HashMap<>();
-    public void addClassToCache(String className) throws Exception {
-        if (!CACHE.containsKey(className)) {
-            System.out.println("Adding to cache: " + className);
-            Class<?> clazz = getClassLoader().loadClass(className, true);
-            CACHE.put(className, clazz);
-            try {
-                CONSTRUCTORS.put(className, clazz.getConstructor());
-            } catch(Exception ex) {
-                // OK
-            }
-        }
-    }
-    public Constructor getConstructorFromCache(String className, Class<?>... parameterTypes) {
-        StringBuilder key = new StringBuilder();
-        key.append(className);
-        for(Class<?> type : parameterTypes) {
-            key.append("_" + type.getName());
-        }
-        return CONSTRUCTORS.get(key.toString());
-    }
-    public void addServiceToCache(String className) throws Exception {
-        Class<?> clazz = getClassLoader().loadClass(className, true);
-        if (!SERVICES.containsKey(clazz)) {
-            ServiceLoader<?> sl = ServiceLoader.load(clazz, moduleClassLoader);
-            List<Object> services = new ArrayList<>();
-            for (Object service : sl) {
-                if (service.getClass().getClassLoader() instanceof ModuleClassLoader) {
-                    services.add(service);
-                }
-            }
-            if (!services.isEmpty()) {
-                SERVICES.put(clazz, services);
-            }
-        }
-    }
-    public List<Object> getServicesFromCache(Class<?> type) {
-        return SERVICES.get(type);
-    }
-
-    public Class<?> getClassFromCache(String className) {
-        return CACHE.get(className);
-    }
-    public Annotation getAnnotation(Class<?> clazz, Class<? extends Annotation> type) {
-        Map<Class<?>, Annotation> map = ANNOTATIONS.get(clazz);
-        //System.out.println("MAP FOR " + clazz + " " + map);
-        if(map == null) {
-            return null;
-        }
-        return ANNOTATIONS.get(clazz).get(type);
-    }
-    public Annotation getAnnotation(Class<?> clazz, Method m, Class<? extends Annotation> type) {
-        Map<Method, Map<Class<?>, Annotation>> map = METHOD_ANNOTATIONS.get(clazz);
-        //System.out.println("MAP FOR " + clazz + " " + map);
-        if(map == null) {
-            return null;
-        }
-        Map<Class<?>, Annotation> map2 = map.get(m);
-        //System.out.println("MAP FOR " + m + " " + map2);
-        if(map2 == null) {
-            return null;
-        }
-        return map2.get(type);
-    }
-    public Annotation[][] getParameterAnnotations(Class<?> clazz, Method m) {
-        Map<Method, Annotation[][]> map = PARAMETERS_ANNOTATIONS.get(clazz);
-        if(map == null) {
-            return null;
-        }
-        Annotation[][] arr = map.get(m);
-        return arr;
-    }
-    public Method[] getDeclaredMethods(Class<?> clazz) {
-        Method[] methods =  METHODS.get(clazz);
-        if(methods == null) {
-            methods = new Method[0];
-        }
-        return methods;
-    }
-    void recordClass(Class clazz) {
-        synchronized (this) {
-            if (!RECORDED_MODULES.isEmpty()) {
-                String className = clazz.getName();
-                if (className.startsWith("java.") || CACHE.containsKey(className)) {
-                    return;
-                }
-                if (RECORDED_MODULES.contains(getName())) {
-                    System.out.println(getName() + " module, recording class: " + className);
-                    CACHE.put(className, clazz);
-                    // Add default constructor if it exists
-                    try {
-                        CONSTRUCTORS.put(className, clazz.getConstructor());
-                    } catch (Exception ex) {
-                        // OK
-                    }
-                    Map<Class<?>, Annotation> map = new HashMap<>();
-                    for (Annotation a : clazz.getAnnotations()) {
-                        Class<? extends Annotation> type = a.annotationType();
-                        System.out.println("Adding annotation " + type + " for annotation " + a);
-                        map.put(type, a);
-                    }
-                    if (!map.isEmpty()) {
-                        ANNOTATIONS.put(clazz, map);
-                    }
-                    if (clazz.getDeclaredMethods().length != 0) {
-                        METHODS.put(clazz, clazz.getDeclaredMethods());
-                        for (final Method method : clazz.getDeclaredMethods()) {
-                            Map<Class<?>, Annotation> ma = new HashMap<>();
-                            for (Annotation a : method.getDeclaredAnnotations()) {
-                                System.out.println("Adding Method annotation " + a + " on " + method.getName());
-                                ma.put(a.annotationType(), a);
-                            }
-                            if (!ma.isEmpty()) {
-                                Map<Method, Map<Class<?>, Annotation>> m = METHOD_ANNOTATIONS.get(clazz);
-                                if (m == null) {
-                                    m = new HashMap<>();
-                                    METHOD_ANNOTATIONS.put(clazz, m);
-                                }
-                                m.put(method, ma);
-                            }
-                            Annotation[][] arr = method.getParameterAnnotations();
-                            if (arr.length == 0) {
-                                arr = new Annotation[0][0];
-                            }
-                            Map<Method, Annotation[][]> mp = PARAMETERS_ANNOTATIONS.get(clazz);
-                            if (mp == null) {
-                                mp = new HashMap<>();
-                                PARAMETERS_ANNOTATIONS.put(clazz, mp);
-                            }
-                            mp.put(method, arr);
-                        }
-                    }
-                }
-            }
-        }
     }
 
     // Linking and resolution
